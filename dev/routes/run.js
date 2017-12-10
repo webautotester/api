1635329e49fc67281499f79c436ea8ae15f3c792
Xavier Blanc <xavier.blanc@labri.fr>
2017-12-10 18:45:40 +0100

    passport.js failure to /login

--- dev/routes/run.js
+++ dev/routes/run.js
@@ -4,7 +4,7 @@ const passport = require('passport');
 
 function init (serverNames, webServer, db, logger) {
 	let router = express.Router();
-	router.use(passport.authenticate('jwt', {session:false}));
+	router.use(passport.authenticate('jwt', {failureRedirect: '/login' , session:false}));
 
 	router
 		.get('/scenario/:sid', (req, res) => {
