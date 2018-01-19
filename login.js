d0f29de158196ab660bab9e0b0deabdd43dff866
Xavier Blanc <xavier.blanc@labri.fr>
2018-01-19 13:00:37 +0100

    HTTP location needs 302

--- login.js
+++ login.js
@@ -219,7 +219,7 @@ function setGitHubOAuthRoute(serverNames, webServer, db, logger  ) {
 				let username = req.user.username;
 				let payload = {username: username};
 				let token = jwt.sign(payload, process.env.JWT_SECRET, {expiresIn:'4h'});
-				res.json({message: 'user authenticated!', username: username, jwt: token});
+				res.status(302).json({message: 'user authenticated!', username: username, jwt: token});
 			} else {
 				res.status(401).json({message:'wrong username / password'});
 			}
